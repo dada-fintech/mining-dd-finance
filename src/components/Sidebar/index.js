82149144e853c98de1d7b7aa5e021c8aced016ac
litianao <exrobbie@gmail.com>
2020-11-12 14:01:38 +0800

    added progress

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,43 +1,49 @@
 import React from 'react'
 import './style.scss'
 
+const supporterList = [
+    {
+        address: '0x98...5643',
+        amount: 393
+    },
+    {
+        address: '0x98...5643',
+        amount: 263
+    },
+    {
+        address: '0x98...5643',
+        amount: 212
+    },
+    {
+        address: '0x98...5643',
+        amount: 198
+    },
+    {
+        address: '0x98...5643',
+        amount: 65
+    },
+]
+
+let totalAmount = 0
+supporterList.map(item => {
+    totalAmount += item.amount
+})
+
 export default function Sidebar() {
     return (<div className="sidebar">
         <div className="block">
             <div className="title">Supporters</div>
-            <div className="box">
-                <div className="box-item">
-                    <div>
-                        37.09%(39.403.85 USDT)
+            <div className="box supporter-box">
+                {supporterList.map(item => (
+                    <div className="box-item">
+                        <div className="progress" style={{width: (item.amount / totalAmount) * 100 + '%'}}></div>
+                        <div className="texts">
+                            <div>{((item.amount / totalAmount) * 100).toFixed(2)}% ({item.amount} USDT)</div>
+                            <div>{item.address}</div>
+                        </div>
+
                     </div>
-                    <div>
-                        0x98...4651
-                    </div>
-                </div>
-                <div className="box-item">
-                    <div>
-                        37.09%(39.403.85 USDT)
-                    </div>
-                    <div>
-                        0x98...4651
-                    </div>
-                </div>
-                <div className="box-item">
-                    <div>
-                        37.09%(39.403.85 USDT)
-                    </div>
-                    <div>
-                        0x98...4651
-                    </div>
-                </div>
-                <div className="box-item">
-                    <div>
-                        37.09%(39.403.85 USDT)
-                    </div>
-                    <div>
-                        0x987s9...4651
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
         <div className="block">
