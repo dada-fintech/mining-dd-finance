3ecdfbcf1ba8c2f409b88bf63a574d185d10a2df
litianao <exrobbie@gmail.com>
2020-11-13 22:18:16 +0800

    changed header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from '../../assets/logo.png'
+import { Button, Tooltip } from 'antd'
+import { useWallet } from 'use-wallet'
+
 
 import './style.scss'
 
 export default function Header() {
+    const wallet = useWallet()
+    useEffect(() => {
+        wallet.connect()
+
+    }, [])
     return (<header className="header">
         <div className="container">
             <img src={Logo} className="logo" />
             <nav>
-                <a>Project</a>
+                <a style={{ marginRight: '8px' }}>Project</a>
+                {wallet.status === 'connected' ? <Tooltip title={window.ethereum.selectedAddress}>
+                    <Button className="btn-green">{window.ethereum.selectedAddress.slice(0, 4) + '...' + window.ethereum.selectedAddress.slice(-4)}</Button>
+                </Tooltip>
+                    : <Button className="btn-green" onClick={() => { wallet.connect() }}>Connect Wallet</Button>}
             </nav>
         </div>
     </header>)
