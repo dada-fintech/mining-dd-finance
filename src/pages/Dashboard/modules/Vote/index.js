ad7092b4ccaa8be176f77a56d1bdc48b19df924c
Bolin <BolinYe@users.noreply.github.com>
2020-11-13 14:49:34 +0800

    v0.1

--- src/pages/Dashboard/modules/Vote/index.js
+++ src/pages/Dashboard/modules/Vote/index.js
@@ -16,7 +16,7 @@ const voteList = [
     {
         title: 'Vote #2',
         due: 'close',
-        date: 'October 05, 2020',
+        date: 'October 03, 2020',
         status: 'pass',
         description: `Unlock Value: <strong>300,000USDT(30%)</strong><br/><br/>Do you approve PIP-7:Unlock 10% in advance for deposit <br/><br/>Since the project has entered the site and the project is progressing rapidly, 10% of the fund originally scheduled to be released on October 10 is about 100000 US dollars, and the deposit needs to be unlocked in advance. Therefore, the proposal is applied<br/><br/>contact address: 0x654asd798FASDF4654sadf411sfasdfFDAds`,
         approve: 70,
@@ -25,7 +25,7 @@ const voteList = [
     {
         title: 'Vote #1',
         due: 'close',
-        date: 'October 05, 2020',
+        date: 'October 03, 2020',
         status: 'fail',
         description: `Unlock Value: <strong>300,000USDT(30%)</strong><br/><br/>Do you approve PIP-7:Unlock 10% in advance for deposit <br/><br/>Since the project has entered the site and the project is progressing rapidly, 10% of the fund originally scheduled to be released on October 10 is about 100000 US dollars, and the deposit needs to be unlocked in advance. Therefore, the proposal is applied<br/><br/>contact address: 0x654asd798FASDF4654sadf411sfasdfFDAds`,
         approve: 70,
@@ -42,7 +42,7 @@ export default function Vote() {
                 <div className="top">
                     <div className="title">{item.title}</div>
                     <div>
-                        <div className="date">{item.due} {item.date} </div>
+                        <div className="date">{item.due} - {item.date} </div>
                         <div className={'status ' + item.status} >
                             {item.status === 'pass' ? <CheckOutlined /> : <CloseOutlined />}{item.status}
                         </div>
