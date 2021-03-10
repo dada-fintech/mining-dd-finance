e6efc2fc0adfd5096f1b0c22736e0d31e54b8c6f
litianao <astrorobbie@163.com>
2021-03-10 11:36:53 +0800

    added formula

--- src/pages/FarmRank/index.js
+++ src/pages/FarmRank/index.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import axios from "utils/axios";
 import Header from "components/Header";
 import { useSelector } from "react-redux";
+import BN from 'bignumber.js'
 import config from "config";
 import "./style.scss";
 
@@ -12,8 +13,37 @@ export default function FarmRank() {
   // const wallet = useWallet()
   const [farmList, setFarmList] = useState([]);
   const [currentTab, setCurrentTab] = useState('eth')
+  const [asset1, setAsset1] = useState('')
+  const [outputAsset1, setOutputAsset1] = useState('')
+  const [outputAsset1Percent, setOutputAsset1Percent] = useState('')
+  const [asset2, setAsset2] = useState('')
+  const [outputAsset2, setOutputAsset2] = useState('')
+  const [outputAsset2Percent, setOutputAsset2Percent] = useState('')
+  const [initPrice, setInitPrice] = useState('')
+  const [goalPrice, setGoalPrice] = useState('')
+
   const network = useSelector((state) => state.setting.network);
   const { i18n, t } = useTranslation();
+
+  useEffect(()=>{
+    const result = new BN(asset1).times(initPrice)
+    console.log(Number(asset1), Number(asset2))
+    setAsset2(Number(result))
+  }, [asset1, initPrice])
+
+  useEffect(() => {
+    const output1Raw = new BN(initPrice).div(goalPrice).sqrt().abs()
+    const output1PercentRaw = new BN(output1Raw).minus(asset1).div(asset1)
+
+    const output2Raw = new BN(asset1).times(asset2).div(output1Raw)
+    const output2PercentRaw = output2Raw.minus(asset2).div(asset2)
+
+    setOutputAsset1(output1Raw.toFixed(2))
+    setOutputAsset1Percent(output1PercentRaw.toFixed(2))
+    setOutputAsset2(output2Raw.toFixed(2))
+    setOutputAsset2Percent(output2PercentRaw.toFixed(2))
+
+  }, [asset1, asset2, goalPrice])
 
   useEffect(() => {
     axios.get("/project/list").then((res) => {
@@ -94,15 +124,15 @@ export default function FarmRank() {
             <div className="form">
               <div className="form-item">
                 <div className="form-label">Input Assset 1</div>
-                <Input />
+                <Input onChange={e => setAsset1(e.target.value) } />
               </div>
               <div className="form-item">
                 <div className="form-label">Input Assset 2</div>
-                <div className="form-value">000</div>
+                <div className="form-value">{asset2}</div>
               </div>
               <div className="form-item">
                 <div className="form-label">Input Price(Asset1/Asset2)</div>
-                <Input />
+                <Input onChange={e => setInitPrice(e.target.value)} />
               </div>
             </div>
 
@@ -110,15 +140,15 @@ export default function FarmRank() {
             <div className="form">
               <div className="form-item">
                 <div className="form-label">Goal Price(Asset1/Asset2)</div>
-                <Input />
+                <Input onChange={e => setGoalPrice(e.target.value)} />
               </div>
               <div className="form-item">
                 <div className="form-label">Output Asset 1</div>
-                <div className="form-value">000 <span className="percent green">+12%</span></div>
+                <div className="form-value">{outputAsset1} <span className="percent green">{outputAsset1Percent}%</span></div>
               </div>
               <div className="form-item">
                 <div className="form-label">Output Asset 2</div>
-                <div className="form-value">000 <span className="percent red">-10%</span></div>
+                <div className="form-value">{outputAsset2} <span className="percent red">{outputAsset2Percent}%</span></div>
               </div>
             </div>
           </div>
